b4612dae3086eee6a86b6d8099f5e01d5d30e349
ProrabVasili <90131636+ProrabVasili@users.noreply.github.com>
2021-10-13 23:22:35 +0300

    Delete p6_pazyuka directory

--- p6_pazyuka/p6_pazyuka_1.py
+++
@@ -1,4 +0,0 @@
-w = ['першу', 'другу']
-a = [set(i.lower() for i in input(f'Введіть {w[i]} фразу: ') if i.isalpha()) for i in range(2)]
-[print(f'Множина унікальних літер {w[i]} фрази: {a[i]}') for i in range(2)]
-print(f'{"Можливо" if a[1].issubset(a[0]) else "Неможливо"} скласти другу фразу з літер першої')
