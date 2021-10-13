f299d35fe39b106e7e266606ccbf210e4d0bd959
Pazyuka Oleg <bumbox2020@gmail.com>
2021-10-13 23:04:06 +0300

    new

--- p6_pazyuka/p6_pazyuka_1.py
+++ p6_pazyuka/p6_pazyuka_1.py
@@ -1,3 +1,4 @@
 w = ['першу', 'другу']
-a,b = [set(i.lower() for i in input(f'Введіть {w[i]} фразу: ') if i.isalpha()) for i in range(2)]
-print(f'{"Можливо" if b.issubset(a) else "Неможливо"} скласти другу фразу з літер першої')
+a = [set(i.lower() for i in input(f'Введіть {w[i]} фразу: ') if i.isalpha()) for i in range(2)]
+[print(f'Множина унікальних літер {w[i]} фрази: {a[i]}') for i in range(2)]
+print(f'{"Можливо" if a[1].issubset(a[0]) else "Неможливо"} скласти другу фразу з літер першої')
